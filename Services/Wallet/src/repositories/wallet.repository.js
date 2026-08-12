557eb44eb8e11afcaff6a5afc957736174176fc6
Pushpen Tiwari <pushpentiwari2005@gmail.com>
2026-08-12 22:49:41 +0530

    imoplement wallet repository

---
+++ Services/Wallet/src/repositories/wallet.repository.js
@@ -0,0 +1,37 @@
+import {prisma} from "../config/prisma.js";
+
+
+class WalletRepository {
+    async createWallet(walletData) {
+        return prisma.wallet.create({
+            data: walletData,
+        });
+    }
+    async findByUserId(userId) {
+    return prisma.wallet.findUnique({
+        where: {
+            userId,
+        },
+    });
+}
+    async findById(id) {
+        return prisma.wallet.findUnique({
+            where: {
+                id,
+            },
+        });
+    }
+    async updateBalance(id, balance) {
+        return prisma.wallet.update({
+            where: {
+                id,
+            },
+            data: {
+                balance: balance,
+            },
+        });
+    }
+    
+}
+
+export default new WalletRepository();      
