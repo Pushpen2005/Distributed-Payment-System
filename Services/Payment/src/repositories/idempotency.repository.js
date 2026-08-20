0a3a570b01b0e95a39d357178ee1917b1e7acddd
Pushpen Tiwari <pushpentiwari2005@gmail.com>
2026-08-21 00:08:41 +0530

    implemented idempotency repository

---
+++ Services/Payment/src/repositories/idempotency.repository.js
@@ -0,0 +1,30 @@
+
+class IdempotencyRepository {
+    async create(tx,data){
+        return tx.idempotencyKey.create({
+            data
+        }); 
+    }
+    async findByUserAndKey(tx, userId, idempotencyKey) {
+        const [record] = await tx.$queryRaw`
+            SELECT *
+            FROM "idempotency_keys"
+            WHERE "userId" = ${userId}
+              AND "idempotencyKey" = ${idempotencyKey}
+            FOR UPDATE
+        `;
+
+        return record ?? null;
+    }
+    async updateStatus(tx, id, status, response) {
+        return tx.idempotencyKey.update({
+            where: {
+                id,
+            },
+            data: {
+                status,
+                response,
+            },
+        });
+    }
+}
