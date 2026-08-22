0132ce253b239e600892958fe1178810434d207c
Pushpen Tiwari <pushpentiwari2005@gmail.com>
2026-08-22 19:58:04 +0530

    fix

--- Services/Payment/src/app.js
+++ Services/Payment/src/app.js
@@ -1,6 +1,10 @@
-import express from "express"
+import express from "express";
+import errorMiddleware from "../../../../shared/middleware/errorMiddleware.js";
 
 const app = express();
+
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 export default app;
