e508a8e498d73e86a2af2f094961bb02ead96cfc
Pushpen Tiwari <pushpentiwari2005@gmail.com>
2026-09-17 23:22:23 +0530

    feat: enhance app.js structure; add health check endpoint and improve middleware usage

--- Services/Wallet/src/app.js
+++ Services/Wallet/src/app.js
@@ -1,8 +1,21 @@
-import express from "express"
+import express from "express";
 import walletRouter from "./routes/wallet.route.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
+
 app.use(express.json());
+
+app.get("/health", (req, res) => {
+    console.log("🔥 HEALTH ENDPOINT HIT");
+
+    res.status(200).json({
+        message: "Wallet service is alive"
+    });
+});
+
 app.use("/wallet", walletRouter);
+
+app.use(errorMiddleware);
 
 export default app;
