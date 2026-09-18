14c2061e019e8a2fd48edf9fe15e87008ade0a2a
Pushpen Tiwari <pushpentiwari2005@gmail.com>
2026-09-18 23:41:03 +0530

    feat: implement PaymentController with transferPayment functionality

---
+++ Services/Payment/src/controllers/payment.controller.js
@@ -0,0 +1,30 @@
+import PaymentService from "../services/payment.service.js";
+
+
+const PaymentController = {
+    async transferPayment(req, res, next) {
+        try {
+            const senderUserId = req.user.sub;
+            const { senderWalletId, receiverWalletId, amount } = req.body;
+            const idempotencyKey = req.headers["idempotency-key"];
+
+            const paymentResult = await PaymentService.transferPayment(
+                senderUserId,
+                senderWalletId,
+                receiverWalletId,
+                amount,
+                idempotencyKey
+            );
+
+            res.status(200).json({
+                success: true,
+                message: "Payment transfer successful",
+                data: paymentResult,
+            });
+        } catch (error) {
+            next(error);
+        }
+    },
+};
+
+export default PaymentController;   
