4b4b3828a085aae4c855908319518a443c362650
Pushpen Tiwari <pushpentiwari2005@gmail.com>
2026-09-26 23:09:12 +0530

    feat(redis): add Redis client with connection handling and shutdown logic

--- Services/Wallet/server.js
+++ Services/Wallet/server.js
@@ -1,11 +1,13 @@
 import 'dotenv/config'; 
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
-
+import  redisClient  from "./src/config/redis.js";
 await connectDB();
 
 const PORT = process.env.PORT || 5000;
+const response = await redisClient.ping();
 
+console.log("Redis PING:", response);
 app.listen(PORT, () => {
   console.log(`Wallet service is running on port ${PORT}`);
 });
